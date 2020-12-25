062299662c0f11f9789f66a56e27cb97cb49b6ce
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2020-12-25 14:52:11 +0100

    do not commit, is safer..

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo timestamping "$1"
+echo timestamping and publishing "$1"...
 hexo publish post "$1"
 ots-cli.js s source/_posts/"$1".md
 mv source/_posts/"$1".md.ots source/_posts/"$1"/
-git add *
-git commit -am "publishing $1"
-git push origin master
+# git add *
+# git commit -am "publishing $1"
+# git push origin master
